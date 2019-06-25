ddc59273e89749b55e927cfcd19eca5ad6842a36
1026593939@qq.com <1026593939@qq.com>
2019-06-25 23:40:27 +0800

    test

---
+++ src/main/scala/com/yx/TEST.scala
@@ -0,0 +1,16 @@
+package com.yx
+
+object TEST {
+
+
+  def main(args: Array[String]): Unit = {
+    val list= Array("yx","lx","czy","hj")
+//    for(i <-list){
+//      println(i)
+//
+//    }
+    list.foreach(i =>println(i))
+  }
+
+
+}
